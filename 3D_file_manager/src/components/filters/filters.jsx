19065aab3a0600b6d258dbdd42e27583993d5829
Alex Rodriguez Benitez <21alexrodri2gmail.com>
2024-10-14 13:17:13 +0200

    test

--- 3D_file_manager/src/components/filters/filters.jsx
+++ 3D_file_manager/src/components/filters/filters.jsx
@@ -1,9 +1,38 @@
 import './filters.css';
+import {useState, useEffect} from 'react';
 function Filters() {
+    const [tags, setTags] = useState([]);
+    const [selectedTag, setSelectedTag] = useState('');
+    const handleCategoryChange = (event) => {
+        setSelectedTag(event.target.value);
+    };
+    useEffect(() => {
+        fetch('http://192.168.116.229/3D_printer/3d_project/query.php')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                setSelectedTag(data);
+            })
+            .catch(error => {
+                console.error('Error fetching categories:', error);
+            });
+    }, []);
     return (
         <div className="filter">
         <h2>Filter</h2>
-        </div>
+        <select value={selectedTag} onChange={handleCategoryChange}>
+            <option value="">Seleccione una categoría</option>
+            {tags.map((tag) => (
+                <option key={tag.id} value={tag.id}>
+                    {tag.tag_name}
+                </option>
+            ))}
+        </select>
+    </div>
     );
 }
 

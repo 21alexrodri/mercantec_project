b5d8e228a2433c2065fe6a8532f55c3a6eba782d
Nil Jimeno Ogaya <joni>
2024-10-15 15:13:48 +0200

    attempt

--- 3D_file_manager/src/components/profile/profile.jsx
+++ 3D_file_manager/src/components/profile/profile.jsx
@@ -3,7 +3,15 @@ import './profile.css';
 function Profile() {
     return (
         <div className="profile">
-        <h2>Profile</h2>
+            <div className='profile_left_column'>
+                <img className="profile_picture" src="" alt="" />
+                <h2 className='username_text'>username</h2>
+                <button className='profile_button' id='button1'>hi</button>
+            </div>
+            <div className='profile_jobs_section'>
+                <h3 className='profile_jobs_section_title'>Your files</h3>
+                <button className='profile_button' id='button2'>hi</button>
+            </div>
         </div>
     );
 }

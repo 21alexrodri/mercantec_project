3a992059c5a147df48018f710f9299d0ad3a54ec
Adrian <acasadoag2@gmail.com>
2024-10-21 15:39:56 +0200

    upload files

--- 3D_file_manager/src/components/new_job/new_job.jsx
+++ 3D_file_manager/src/components/new_job/new_job.jsx
@@ -1,18 +1,53 @@
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState, useEffect, useRef} from "react";
 import "./new_job.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faUpload} from '@fortawesome/free-solid-svg-icons';
 
 export const NewJob = ({closeNewJob})=>{
     const [files,setFiles] = useState([])
+    const imgUploadContainerRef = useRef(null)
+    const fileInputRef = useRef(null)
 
     const handleContainerClick = (e) => {
         e.stopPropagation();
     };
 
-    const handlePhotoUpload = () => {
+    // ESTO ESTÁ MAL, NO ELIMINA EL FILE DEL ARRAY !!!!!!!!
 
+    const handleDeleteFile = (e) => {
+        e.target.style.display = "none"
+        for(let file in files){
+
+        }
     };
+
+    const handleImgChange = (e) => {
+        const img = e.target.files[0]
+        if(img){
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                imgUploadContainerRef.current.style.backgroundImage = `url(${reader.result})`;
+            };
+            reader.readAsDataURL(img); 
+        }
+    }
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0]
+        if(file){
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                let newFile = `url(${reader.result})`
+                setFiles((prevFiles) => [...prevFiles,{"url" : newFile, "name" : file.name}])
+            }
+            reader.readAsDataURL(file);
+        }
+    }
+
+    const handleClearImg = () => {
+        fileInputRef.current.value = ""
+        imgUploadContainerRef.current.style.backgroundImage = "none"
+    }
 
     return(
         <>
@@ -23,9 +58,17 @@ export const NewJob = ({closeNewJob})=>{
                     </div>
                     <div className="main">
                         <div className="form-container">
-                            <div className="img-upload" onClick={handlePhotoUpload}>
-                                <FontAwesomeIcon className="upload-icon" icon={faUpload}/>
+                            <div className="img-upload-manager">
+                                <div ref={imgUploadContainerRef} className="img-upload-container">
+                                    <label className="img-upload-label" htmlFor="img-upload">
+                                        <FontAwesomeIcon className="upload-icon" icon={faUpload}/>
+                                        <input ref={fileInputRef} id="img-upload" className="img-upload" type="file" onChange={handleImgChange} accept="image/jpg, image/png, image/jpeg"/>
+                                    </label>
+                                </div>
+                                <p>* Only .jpg, .png and .jpeg accepted</p>
+                                <button class="nj-delete-image" onClick={handleClearImg}>Delete image</button>
                             </div>
+                                
                             <div className="nj-form">
                                 <label className="needed nj-label">
                                     <input className="project-name" type="text" placeholder="Project Name..."/>
@@ -62,12 +105,14 @@ export const NewJob = ({closeNewJob})=>{
                             </div>
                         </div>
                         <div className="files-upload">
-                            <ul>
-                                {files.map((file,index) => (
-                                    <li></li>
+                            <ul class="files-list">
+                                {files.map((file, index) => (
+                                    <li onClick={handleDeleteFile} className="nj-file">
+                                        {file.name}
+                                    </li>
                                 ))}
-                                <li className="new-file">
-                                    
+                                <li className="nj-file new-file">
+                                    <input type="file" onChange={handleFileChange}/>
                                 </li>
                             </ul>
                         </div>

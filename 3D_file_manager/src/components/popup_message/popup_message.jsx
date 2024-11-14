c7ca3f4eb15cd0484dbf8d750d9b7ebaece77bd9
21alexrodri <21alexrodri@gmail.com>
2024-11-14 13:02:04 +0100

    test

---
+++ 3D_file_manager/src/components/popup_message/popup_message.jsx
@@ -0,0 +1,38 @@
+import './popup_message.css';
+import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+
+export const Popup = ({ message, status }) => {
+    const { t } = useTranslation();
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setVisible(false);
+        }, 3000);
+
+        return () => clearTimeout(timeout);
+    }, []);
+    
+    const closePopup = () => {
+        setVisible(false);
+    };
+
+    if (!visible) return null;
+
+    return (
+        <div className={`popup-message-container status_${status}`}>
+            <div className="popup-message" onClick={(e) => e.stopPropagation()}>
+                <div className="popup-message-header">
+                    <FontAwesomeIcon icon={faTimes} className="popup-message-close" onClick={closePopup} />
+                </div>
+                <div className="popup-message-body">
+                    <p>{message}</p>
+                </div>
+                <div className="progress-bar"></div>
+            </div>
+        </div>
+    );
+};
